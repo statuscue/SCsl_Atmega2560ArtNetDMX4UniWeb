aadb6e60feaff27fdbbabc9ba9e315a3047bc924
Eugene Mikhaylov <statuscue@hes.od.ua>
2019-03-09 16:38:11 +0200

    Add project files.

---
+++ SCsl_Atmega2560ArtNetDMX4UniWeb.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SCsl_Atmega2560ArtNetDMX4UniWeb.ino
+ Created:	09-Mar-19 16:38:06
+ Author:	statu
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
