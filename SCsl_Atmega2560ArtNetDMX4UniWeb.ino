57bc09f5f1bc909c52dfd7cfc42b73fbfb812876
Eugene Mikhaylov <statuscue@hes.od.ua>
2019-03-09 16:40:24 +0200

    Init

--- SCsl_Atmega2560ArtNetDMX4UniWeb.ino
+++ SCsl_Atmega2560ArtNetDMX4UniWeb.ino
@@ -1,7 +1,13 @@
 /*
  Name:		SCsl_Atmega2560ArtNetDMX4UniWeb.ino
  Created:	09-Mar-19 16:38:06
- Author:	statu
+ Author:	statuscue
+ Editor:	http://hes.od.ua
+*/
+
+/*
+	Copyright 2019-2019 by Yevhen Mykhailov
+	Art-Net(TM) Designed by and Copyright Artistic Licence Holdings Ltd.
 */
 
 // the setup function runs once when you press reset or power the board
